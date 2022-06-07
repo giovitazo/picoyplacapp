cb4f0d29f32ee6b78224874177aec1cdb36122d9
Giovanny Galarza <giovanny.galarza.ec@outlook.com>
2022-06-06 23:29:59 -0500

    forms now modify the state of the app and the reset button empties forms and states

--- src/components/FormElement.js
+++ src/components/FormElement.js
@@ -3,10 +3,16 @@ import "../scss/formElement.scss";
 
 export default class FormElement extends Component {
   render() {
+    const { stateChange } = this.props;
     return (
       <div className="formElement">
         <label> {this.props.label} </label>
-        <input type={this.props.type} placeholder={this.props.placeHolder} />
+        <input
+          type={this.props.type}
+          placeholder={this.props.placeHolder}
+          onChange={stateChange}
+          id={this.props.statePart}
+        />
         <span id="plateNumberSpan" className="errorDisplay"></span> <br />
       </div>
     );
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,25 +3,69 @@ import Result from "../components/Result";
 import DataInput from "./DataInput";
 import FormElement from "../components/FormElement";
 import logo from "../img/Logo.png";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <img src={logo} alt="PICOYPLACapp" />
-      </header>
-      <Result />
-      <DataInput>
-        <FormElement label="Plate number:" placeHolder="PSC0610" type="text" />
-        <FormElement label="Date:" placeHolder="MM/DD/YYYY" type="text" />
-        <FormElement label="Time:" type="time" />
-        <div className="buttonPlate">
-          <button>Reset</button>
-          <button>Predict</button>
-        </div>
-      </DataInput>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      plateNumber: "",
+      date: "",
+      time: "",
+      allDataOk: false,
+    };
+  }
+  onReset = () => {
+    let inputs = document.querySelectorAll("input");
+    inputs.forEach((element) => {
+      element.value = "";
+    });
+    this.setState({ plateNumber: "", time: "", date: "" });
+  };
+  onStateChange = (event) => {
+    let stateObject = function () {
+      let returnObj = {};
+      returnObj[event.target.id] = this.target.value;
+      return returnObj;
+    }.bind(event)();
+    this.setState(stateObject);
+  };
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <img src={logo} alt="PICOYPLACapp" />
+        </header>
+        <Result />
+        <DataInput>
+          <FormElement
+            label="Plate number:"
+            placeHolder="PSC0610"
+            type="text"
+            statePart="plateNumber"
+            stateChange={this.onStateChange}
+          />
+          <FormElement
+            label="Date:"
+            placeHolder="MM/DD/YYYY"
+            type="text"
+            statePart="date"
+            stateChange={this.onStateChange}
+          />
+          <FormElement
+            label="Time:"
+            statePart="time"
+            stateChange={this.onStateChange}
+            type="time"
+          />
+          <div className="buttonPlate">
+            <button onClick={this.onReset}>Reset</button>
+            <button>Predict</button>
+          </div>
+        </DataInput>
+      </div>
+    );
+  }
 }
 
 export default App;
